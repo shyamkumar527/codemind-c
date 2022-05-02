770c5ea0a8ba4746f1542c59d57fbbf0bf0af845
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-05-02 22:32:27 +0530

    #include<stdio.h> int main() { 	int i,j,r; 	scanf("%d",&r); 	for(i=0;i<r;i++) 	{ 		for(j=0;j<r;j++) 		{ 			printf("%c ",'A'+i); 		} 		printf(" "); 	} 	return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        for(j=1;j<=i;j++)
-        {
-            printf("%d",j);
-        }
-        printf("
+	int i,j,r;
+	scanf("%d",&r);
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<r;j++)
+		{
+			printf("%c ",'A'+i);
+		}
+		printf("
 ");
-    }
+	}
+	return 0;
 }
