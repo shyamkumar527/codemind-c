e36e3f9ffb3fac79037c9a54361c511259bc72ba
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-05-02 23:08:13 +0530

    #include<stdio.h> int main() { 	int i; 	char str[100]; 	scanf("%[^ ]s",str); 	for(i=0;str[i]!=NULL;i++) 	{ 		if(str[i]>='A' && str[i]<='Z') 		{ 			str[i]+=32; 		} 	} 	printf("%s",str); }

---
+++ To_Lower_Case.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	char str[100];
+	scanf("%[^
+]s",str);
+	for(i=0;str[i]!=NULL;i++)
+	{
+		if(str[i]>='A' && str[i]<='Z')
+		{
+			str[i]+=32;
+		}
+	}
+	printf("%s",str);
+}
