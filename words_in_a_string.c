b09706fb96a9e85847c1e7113f084fcabc903667
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-05-14 14:07:06 +0530

    #include<stdio.h> int main(){     char str[1000];     int i=0,count=0;     scanf("%[^ ]s", str);     for(i;str[i]!=NULL;i++){         if(str[i]==' '){             count++;         }     }     printf("%d",count+1); }

---
+++ words_in_a_string.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char str[1000];
+    int i=0,count=0;
+    scanf("%[^
+]s", str);
+    for(i;str[i]!=NULL;i++){
+        if(str[i]==' '){
+            count++;
+        }
+    }
+    printf("%d",count+1);
+}
