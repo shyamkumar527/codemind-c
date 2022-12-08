fa7270ec500e2c0f1d0eba622db59f206140fa51
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-12-08 21:47:02 +0530

    Update Majority_Element.c

--- Majority_Element.c
+++ Majority_Element.c
@@ -6,34 +6,27 @@ int main()
     int n,i,j,max=0,ans;
     scanf("%d",&n);
     int arr[n];
-	for(i=0;i<n;i++)
-	{
+	for(i=0;i<n;i++){
 		scanf("%d",&arr[i]);
 	} 
     int length=sizeof(arr)/sizeof(arr[0]);  
       
-    for(i=0;i<length;i++)
-	{  
+    for(i=0;i<length;i++){  
         int count=0;
-		for(j=0;j<length;j++)
-		{  
-            if(arr[i]==arr[j] && i!=j)
-			{
-				count++;
-			} 
+	for(j=0;j<length;j++){  
+            if(arr[i]==arr[j] && i!=j){
+			count++;
+		} 
         }
-		if(count>max)
-		{
+	if(count>max){
+		ans=arr[i];
+		max=count;
+	}
+	else if(count==max){
+		if(ans>arr[i]){
 			ans=arr[i];
-			max=count;
 		}
-		else if(count==max)
-		{
-			if(ans>arr[i])
-			{
-				ans=arr[i];
-			}
-		}
+	}
     }
     printf("%d",ans);  
 }
